54b3cee30bb264e63a5e3844699a099ee8b29da9
GiangHieuToiDay <ganrt.hieu.08@gmail.com>
2026-06-09 10:33:58 +0700

    Update Mapper Post-service

--- services/post-service/src/main/java/com/social_media/postservice/infrastructure/mapper/BookmarkMapper.java
+++ services/post-service/src/main/java/com/social_media/postservice/infrastructure/mapper/BookmarkMapper.java
@@ -2,7 +2,9 @@ package com.social_media.postservice.infrastructure.mapper;
 
 import com.social_media.postservice.domain.aggreate.Bookmark;
 import com.social_media.postservice.infrastructure.entity.BookmarkEntity;
+import org.mapstruct.Mapper;
 
+@Mapper(componentModel = "spring")
 public interface BookmarkMapper {
 
     BookmarkEntity toEntity(Bookmark domain);
--- services/post-service/src/main/java/com/social_media/postservice/infrastructure/mapper/ReportMapper.java
+++ services/post-service/src/main/java/com/social_media/postservice/infrastructure/mapper/ReportMapper.java
@@ -2,7 +2,9 @@ package com.social_media.postservice.infrastructure.mapper;
 
 import com.social_media.postservice.domain.aggreate.Report;
 import com.social_media.postservice.infrastructure.entity.ReportEntity;
+import org.mapstruct.Mapper;
 
+@Mapper(componentModel = "spring")
 public interface ReportMapper {
 
     ReportEntity toEntity(Report domain);
