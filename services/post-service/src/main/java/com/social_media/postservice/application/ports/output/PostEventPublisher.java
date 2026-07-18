168f7e2b7ca0e270d75dd01fea0210b15d30fb29
GiangHieuToiDay <ganrt.hieu.08@gmail.com>
2026-07-18 10:27:45 +0700

    update kafka delete post

--- services/post-service/src/main/java/com/social_media/postservice/application/ports/output/PostEventPublisher.java
+++ services/post-service/src/main/java/com/social_media/postservice/application/ports/output/PostEventPublisher.java
@@ -1,7 +1,9 @@
 package com.social_media.postservice.application.ports.output;
 
 import com.social_media.postservice.application.dto.events.PostCreatedIntegrationEvent;
+import com.social_media.postservice.application.dto.events.PostDeleteIntegrationEvent;
 
 public interface PostEventPublisher {
     void publishPostCreated(PostCreatedIntegrationEvent event);
+    void publishPostDelete(PostDeleteIntegrationEvent event);
 }
--- services/post-service/src/main/java/com/social_media/postservice/infrastructure/mesaging/kafka/publisher/KafkaPostEventPublisher.java
+++ services/post-service/src/main/java/com/social_media/postservice/infrastructure/mesaging/kafka/publisher/KafkaPostEventPublisher.java
@@ -3,6 +3,7 @@ package com.social_media.postservice.infrastructure.mesaging.kafka.publisher;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.social_media.postservice.application.dto.events.PostCreatedIntegrationEvent;
+import com.social_media.postservice.application.dto.events.PostDeleteIntegrationEvent;
 import com.social_media.postservice.application.ports.output.PostEventPublisher;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -17,6 +18,7 @@ public class KafkaPostEventPublisher implements PostEventPublisher {
     private final KafkaTemplate<String, Object> kafkaTemplate;
 
     private final String TOPIC = "post-created-topic";
+    private final String TOPIC2 = "post-deleted-topic";
 
 
 
@@ -36,4 +38,23 @@ public class KafkaPostEventPublisher implements PostEventPublisher {
 
                 });
     }
+
+    @Override
+    public void publishPostDelete(PostDeleteIntegrationEvent event) {
+        log.info("Post Delete Event Send: {}", event.getId());
+
+        kafkaTemplate.send(TOPIC2, event)
+                .whenComplete((res, ex) -> {
+                    if (ex == null) {
+                        log.info("Post Delete Event Send Success: {}", event.getId());
+                        log.info("Event: {}", event);
+                    }else  {
+                        log.error("Post Delete Event Send Failed: {}", ex.getMessage());
+                    }
+                });
+
+    }
+
+
+
 }
